4614675e1a2a00b5fcd501818b5b04678ab03386
kefaslungu <jameskefaslungu@gmail.com>
2022-09-09 03:32:03 +0100

    I keep getting some check fail, so just get use to minor update for 0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-	/**
-	 * main - Entry point
-	*
-	* Return: Always 0 (Success)
-	 */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-	{
+{
 	int a;
 	long int b;
 	long long int c;
@@ -19,4 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-	}
+}
