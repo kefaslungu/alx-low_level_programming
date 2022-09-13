6260a2cdbda4dc6ed315c0b62f5be5d33085e98f
kefaslungu <jameskefaslungu@gmail.com>
2022-09-13 18:22:10 +0100

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs 
+ * 
+ * Description: This function returns absolute value of a number
+ * Return: Absolute value of number
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
