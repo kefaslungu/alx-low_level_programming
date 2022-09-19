43f80f2b4d969f488965aa34ff8b36ece2aa1a95
kefaslungu <jameskefaslungu@gmail.com>
2022-09-19 23:18:59 +0100

    this is the  final push for 0x05-pointers_arrays_strings

--- 0x04-more_functions_nested_loops/git.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-git config --global user.email "jameskefaslungu@gmail.com"
-git config --global user.name "kefaslungu"
-git add .
-git commit -m '0x04-more_functions_nested_loops'
-git push
