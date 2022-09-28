f7d8551a3d8bce79330ed639828b057fceade265
kefaslungu <jameskefaslungu@gmail.com>
2022-09-28 18:40:29 +0100

    this is the  first push for 0x08-recursion

--- 0x08-recursion/New folder/3-factorial.c
+++ 0x08-recursion/New folder/3-factorial.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * factorial c tutorial: 5 - (5 - 1)
+ * factorial - entry point
  * @n: input
  * Return: Always 0 (Success)
  */
 int factorial(int n)
 {
+
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
