733cb390027420d03bf056324ec5e430f4e11f3b
kefaslungu <jameskefaslungu@gmail.com>
2022-10-10 22:38:01 +0100

    final commit for 0x0D. C - Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-
-#endif /* FUNCTION_LIKE_MACRO */
+#ifndef _FUNC_LIKE_MACRO_H
+#define _FUNC_LIKE_MACRO_H
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
