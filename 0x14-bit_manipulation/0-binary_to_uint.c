9050d443539bbe920b898ba4c02b89a23193d8ea
kefaslungu <jameskefaslungu@gmail.com>
2022-10-27 22:08:10 +0100

    first commit for the 0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,32 @@
 #include "main.h"
-
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	if (b[len] == '\0')
+	if (b == NULL)
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
+	while (b[i + 1])
 	{
-		num <<= 1;
-		num += b[len] - '0';
-		len++;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
 	}
 
-	return (num);
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
