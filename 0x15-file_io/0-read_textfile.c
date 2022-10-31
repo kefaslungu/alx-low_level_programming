60b8cc5912a9845443bfc0bd66cba9cdccda852e
kefaslungu <jameskefaslungu@gmail.com>
2022-10-31 23:17:52 +0100

    first commit for the 0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,39 @@
 #include "main.h"
 
 /**
-* read_textfile - check the code for Holberton School students.
-* @filename: file to read and write
-* @letters: number of letters to read and write.
-* Return: letters printed
-*/
-
+ * read_textfile - Entry Point
+ * @filename: file name
+ * @letters: size
+ * Return: 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nletters;
-	int file;
-	char *text;
+	int file, rd, wr;
+	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-	text = malloc(sizeof(char) * letters + 1);
-	if (text == NULL)
-		return (0);
+
 	file = open(filename, O_RDONLY);
+
 	if (file == -1)
-	{
-		free(text);
 		return (0);
-	}
-	nletters = read(file, text, sizeof(char) * letters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(file);
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
 		return (0);
-	}
-	nletters = write(STDOUT_FILENO, text, nletters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(file);
+
+	rd = read(file, buf, letters);
+	if (rd == -1)
 		return (0);
-	}
-	free(text);
+
+	buf[letters] = '\0';
+
+	wr = write(1, buf, rd);
+	if (wr == -1)
+		return (0);
+
 	close(file);
-	return (nletters);
+	free(buf);
+	return (wr);
 }
